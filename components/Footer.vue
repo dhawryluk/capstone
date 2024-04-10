407fce6ece6322433a3cc418dbaa841db4b649b2
DKH <derek.hawryluk@edu.sait.ca>
2024-04-10 12:33:44 -0600

    footer setup

---
+++ components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="w-full mt-14 bg-[url('/assets/images/wave.jpg')] bg-cover bg-center rounded-lg"
+  >
+    <div
+      class="w-1/4 bg-white p-4 font-bold opacity-30 grid grid-cols-2 justify-evenly"
+    >
+      <div
+        class="flex flex-col gap-4 bg-[url('/assets/images/wave.jpg')] bg-clip-text text-transparent"
+      >
+        <h2 class="text-4xl">Where</h2>
+        <h2 class="text-4xl">Gamers</h2>
+        <h2 class="text-4xl">Unite</h2>
+      </div>
+      <div
+        class="flex flex-col justify-self-end text-5xl bg-[url('/assets/images/wave.jpg')] bg-clip-text text-transparent"
+      >
+        <p>4</p>
+        <p>P</p>
+        <p>L</p>
+        <p>A</p>
+        <p>Y</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
