ec31feee811b7329b20923ca27bc73e249a709c8
DKH <derek.hawryluk@gmail.com>
2025-03-20 09:32:36 -0600

    updating about page

--- components/PrimaryButton.vue
+++ components/PrimaryButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <button
-      class="rounded-md border-solid border-2 border-accent1 dark:border-accent2 text-lightColor p-2 font-serif bg-secondary dark:hover:bg-accent2 dark:hover:text-darkColor"
+      class="rounded-md border-solid border-2 border-accent1 dark:border-accent2 text-darkColor dark:text-lightColor p-2 font-serif bg-secondary/20 dark:bg-secondary hover:bg-accent1 dark:hover:bg-accent2 hover:text-lightColor dark:hover:text-darkColor"
     >
       <slot>Read More</slot>
     </button>
--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="px-4 flex flex-col h-full">
-    <img src="/assets/images/city.jpg" class="rounded-t-xl bg-center" />
-    <div class="bg-secondary p-6 rounded-b-xl">
-      <h1 class="text-4xl font-sans pb-6">About 4Play</h1>
+    <div class="rounded-t-xl overflow-hidden">
+      <img
+        src="/assets/images/city.jpg"
+        class="w-full h-[700px] aspect-auto object-cover object-bottom"
+        alt="City background"
+      />
+    </div>
+    <!-- Adjust padding for content -->
+    <div class="bg-secondary p-4 sm:p-6 rounded-b-xl">
+      <h1 class="text-2xl sm:text-4xl font-sans pb-4 sm:pb-6">About 4Play</h1>
       <div>
         <p>
           Welcome to 4Play, the ultimate destination where gamers from all
@@ -11,7 +18,7 @@
           of gaming, where players come together to connect, share experiences,
           and revel in the excitement of their favorite titles.
         </p>
-        <h2 class="text-2xl font-sans py-6">Our Mission</h2>
+        <h2 class="text-xl sm:text-2xl font-sans py-4 sm:py-6">Our Mission</h2>
         <p>
           Our mission at 4Play is simple yet profound: to provide a dynamic
           platform where gamers can unite, engage, and immerse themselves in the
@@ -20,7 +27,9 @@
           together to share their love for gaming, regardless of their skill
           level or preferred genre.
         </p>
-        <h2 class="text-2xl font-sans py-6">What Sets Us Apart</h2>
+        <h2 class="text-xl sm:text-2xl font-sans py-4 sm:py-6">
+          What Sets Us Apart
+        </h2>
         <p>
           Unlike traditional gaming forums or social media platforms, 4Play is
           exclusively dedicated to gaming. Whether you're a casual gamer, a
@@ -30,7 +39,7 @@
           their gaming experiences, and stay up-to-date on the latest news and
           trends in the gaming industry.
         </p>
-        <h2 class="text-2xl font-sans py-6">Features</h2>
+        <h2 class="text-xl sm:text-2xl font-sans py-4 sm:py-6">Features</h2>
         <ul class="list-disc pb-4 pl-4">
           <li>
             Community Interaction: Join lively discussions, comment on posts,
@@ -55,7 +64,7 @@
             enthusiasm for specific games or genres.
           </li>
         </ul>
-        <h3 class="text-xl italic pt-4">
+        <h3 class="text-lg sm:text-xl italic pt-4">
           Join the 4Play Community Today Whether you're looking to connect with
           fellow gamers, share your gaming triumphs, or simply stay informed
           about the latest happenings in the gaming world, 4Play is your go-to
