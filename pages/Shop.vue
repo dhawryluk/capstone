39a5161619f8c1d1d83087d0de2b4fa65a88b55d
DKH <derek.hawryluk@gmail.com>
2025-04-21 15:22:19 -0600

    updating shop

--- pages/Shop.vue
+++ pages/Shop.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col items-center px-4">
-    <h1 class="text-4xl md:text-6xl font-bold text-center mb-6">Shop</h1>
+    <h1 class="w-full md:w-5/6 dark:bg-gradient-to-r dark:from-secondary dark:via-accent2 dark:to-darkColor bg-gradient-to-r from-lightColor via-accent2 to-white text-lightColor rounded-lg text-2xl md:text-6xl font-bold ring-2 ring-accent1 dark:ring-accent2 text-center mb-8 py-4 font-sans drop-shadow-lg">Shop</h1>
     <div
       class="w-full md:w-5/6 m-auto flex flex-wrap items-center justify-center gap-4"
     >
@@ -10,37 +10,32 @@
         :stripe-account="accountId"
       ></stripe-buy-button>
       <stripe-buy-button
-        buy-button-id="buy_btn_1RDtO8DHPkLwKHSucUxbXLYu"
+        buy-button-id="buy_btn_1RGPXmDHPkLwKHSu7HvFydTz"
         :publishable-key="publishableKey"
         :stripe-account="accountId"
       ></stripe-buy-button>
       <stripe-buy-button
-        buy-button-id="buy_btn_1RDtO8DHPkLwKHSucUxbXLYu"
+        buy-button-id="buy_btn_1RGPT6DHPkLwKHSuf4mW2f3r"
         :publishable-key="publishableKey"
         :stripe-account="accountId"
       ></stripe-buy-button>
       <stripe-buy-button
-        buy-button-id="buy_btn_1RDtO8DHPkLwKHSucUxbXLYu"
+        buy-button-id="buy_btn_1RGPbeDHPkLwKHSubtz4eROM"
         :publishable-key="publishableKey"
         :stripe-account="accountId"
       ></stripe-buy-button>
       <stripe-buy-button
-        buy-button-id="buy_btn_1RDtO8DHPkLwKHSucUxbXLYu"
+        buy-button-id="buy_btn_1RGPdxDHPkLwKHSu3pS8uxtl"
         :publishable-key="publishableKey"
         :stripe-account="accountId"
       ></stripe-buy-button>
       <stripe-buy-button
-        buy-button-id="buy_btn_1RDtO8DHPkLwKHSucUxbXLYu"
+        buy-button-id="buy_btn_1RGPfVDHPkLwKHSuMWGy161i"
         :publishable-key="publishableKey"
         :stripe-account="accountId"
       ></stripe-buy-button>
       <stripe-buy-button
-        buy-button-id="buy_btn_1RDtO8DHPkLwKHSucUxbXLYu"
-        :publishable-key="publishableKey"
-        :stripe-account="accountId"
-      ></stripe-buy-button>
-      <stripe-buy-button
-        buy-button-id="buy_btn_1RDtO8DHPkLwKHSucUxbXLYu"
+        buy-button-id="buy_btn_1RGPhGDHPkLwKHSuRUwjaz0I"
         :publishable-key="publishableKey"
         :stripe-account="accountId"
       ></stripe-buy-button>
